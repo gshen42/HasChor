99eb56653f9a111099aad82a041a37ec31bb6661
Gan Shen <gan_shen@icloud.com>
2023-02-09 15:28:39 -0800

    new `Choreo` modulo `epp` and remove `Local`

--- src/Choreography/Choreo.hs
+++ src/Choreography/Choreo.hs
@@ -1,38 +1,53 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs     #-}
+
 --------------------------------------------------------------------------------
 -- Monads for writing choreographies.
 --------------------------------------------------------------------------------
-module Choreography.Choreo (Choreo, comm, epp) where
 
-import Choreography.Control
+module Choreography.Choreo where
+
 import Choreography.Location
-import Control.Concurrent.Async
+import Control.Monad.Freer
+import Data.Proxy
+import GHC.TypeLits
 
-data Choreo a where
-    Pure :: a -> Choreo a
-    Comm :: (Show x, Read x) => x -> Location -> Location -> (x -> Choreo a) -> Choreo a
+newtype a @ (l :: Symbol) = Wrap a
 
-instance Functor Choreo where
-    fmap f (Pure a)       = Pure (f a)
-    fmap f (Comm x s r k) = Comm x s r (fmap f . k)
+wrap :: a -> a @ l
+wrap = Wrap
 
-instance Applicative Choreo where
-    pure = Pure
-    f <*> x = f >>= \f -> x >>= \x -> return (f x)
+unwrap :: a @ l -> a
+unwrap (Wrap a) = a
 
-instance Monad Choreo where
-    (Pure a)       >>= f = f a
-    (Comm x s r k) >>= f = Comm x s r (\x -> k x >>= f)
+type Unwrap l = forall a. a @ l -> a
 
-runChoreo :: Choreo a -> a
-runChoreo (Pure a)     = a
-runChoreo (Comm x s r k) = runChoreo (k x)
+-- I really want to put the constraint (Monad m) in the definition, but that
+-- needs -XDatatypeContexts and is considered a bad practice, why? also it
+-- doens't work with GADT
+data ChoreoSig m a where
+  Local :: Proxy l -> (Unwrap l -> m a) -> ChoreoSig m (a @ l)
+  Comm  :: Proxy l -> a @ l -> Proxy l' -> ChoreoSig m (a @ l')
+  -- ^ TODO: ensure l and l' are different
 
-comm :: (Show x, Read x) => x -> Location -> Location -> Choreo x
-comm x s r = Comm x s r Pure
+type Choreo m = Freer (ChoreoSig m)
 
-epp :: Choreo a -> Location -> Control a
-epp (Comm x s r k) l
-    | l == s    = send x r >> epp (k x) l
-    | l == r    = recv s >>= \r -> epp (k r) l
-    | otherwise = epp (k x) l
-epp (Pure a) l = return a
+locallyDo :: Proxy l -> (Unwrap l -> m a) -> Choreo m (a @ l)
+locallyDo l m = toFreer (Local l m)
+
+(~>) :: (Proxy l, a @ l) -> Proxy l' -> Choreo m (a @ l')
+(~>) (l, a) l' = toFreer (Comm l a l')
+
+runChoreo :: Monad m => Choreo m a -> m a
+runChoreo = runFreer alg
+  where
+    alg :: Monad m => ChoreoSig m a -> m a
+    alg (Local _ m)   = wrap <$> m unwrap
+    alg (Comm _ a _ ) = return $ (wrap . unwrap) a
+
+-- epp :: Choreo a -> Location -> Control a
+-- epp (Comm x s r k) l
+--     | l == s    = send x r >> epp (k x) l
+--     | l == r    = recv s >>= \r -> epp (k r) l
+--     | otherwise = epp (k x) l
+-- epp (Pure a) l = return a
