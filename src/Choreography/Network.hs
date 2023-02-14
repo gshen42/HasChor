72e312bd342a975196b60002fe6e524c039630c1
Gan Shen <gan_shen@icloud.com>
2023-02-13 16:59:21 -0800

    add broadcast to `Network` and some tweaks

--- src/Choreography/Network.hs
+++ src/Choreography/Network.hs
@@ -9,28 +9,32 @@ module Choreography.Network where
 
 import Choreography.Location
 import Control.Concurrent.Chan
-import Data.HashMap.Strict (HashMap, (!), insert, empty)
+import Data.HashMap.Strict (HashMap, (!), insert, empty, keys)
 import Control.Monad
 import Control.Monad.Freer
 import Control.Monad.IO.Class
 
 data NetworkSig m a where
-  Loca :: m a -> NetworkSig m a
-  Send :: Show a => a -> Location l -> NetworkSig m ()
-  Recv :: Read a => Location l -> NetworkSig m a
+  Run   :: m a -> NetworkSig m a
+  Send  :: Show a => a -> LocTm -> NetworkSig m ()
+  Recv  :: Read a => LocTm -> NetworkSig m a
+  BCast :: Show a => a -> NetworkSig m ()
 
 type Network m = Freer (NetworkSig m)
 
-loca :: m a -> Network m a
-loca m = toFreer $ (Loca m)
+run :: m a -> Network m a
+run m = toFreer $ Run m
 
-send :: Show a => a -> Location l -> Network m ()
+send :: Show a => a -> LocTm -> Network m ()
 send a l = toFreer $ Send a l
 
-recv :: Read a => Location l -> Network m a
+recv :: Read a => LocTm -> Network m a
 recv l = toFreer $ Recv l
 
-data Context = forall l. Context
+broadcast :: Show a => a -> Network m ()
+broadcast a = toFreer $ BCast a
+
+data Context = Context
   { sendChan :: Chan (LocTm, String)
   , recvChans :: HashMap LocTm (Chan String)
   }
@@ -47,6 +51,10 @@ mkContext ls = do
       c <- newChan
       return $ insert l c hm
 
+-- all the locations in a context, needed to implement broadcasting
+locs :: Context -> [LocTm]
+locs = keys . recvChans
+
 -- interpet a `Network` program as a `IO` program with access to a `Context`
 -- how `Context` is manipulated is determined in the next phase, this allows
 -- us to have multiple implementations of `Network` while reusing most of the
@@ -55,6 +63,7 @@ runNetworkMain :: MonadIO m => Context -> Network m a -> m a
 runNetworkMain ctx = runFreer alg
   where
     alg :: MonadIO m => NetworkSig m a -> m a
-    alg (Loca m)   = m
-    alg (Send a l) = liftIO $ writeChan (sendChan ctx) (toLocTm l, show a)
-    alg (Recv l)   = liftIO $ read <$> readChan (recvChans ctx ! toLocTm l)
+    alg (Run m)    = m
+    alg (Send a l) = liftIO $ writeChan (sendChan ctx) (l, show a)
+    alg (Recv l)   = liftIO $ read <$> readChan (recvChans ctx ! l)
+    alg (BCast a)  = mapM_ alg $ fmap (Send a) (locs ctx)
