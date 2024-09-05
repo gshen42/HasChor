39a9a02c090c208ef7c969be0e42254c6e87ce12
Gan Shen <gan_shen@icloud.com>
2024-09-05 12:39:02 -0700

    some progress toward `epp`

--- src/Choreography/Choreo.hs
+++ src/Choreography/Choreo.hs
@@ -10,38 +10,37 @@ import Choreography.Network hiding (Sig)
 import Control.Monad.Async
 import Control.Monad.Freer
 import Control.Monad.IO.Class
+import Control.Monad.Identity
 import Control.Monad.State.Strict
 import Data.HashMap.Strict as HM
-
--- * The Choreo monad
 
 -- | A different notation that might be more readable: `a @ l` is an asynchronous
 -- computation of type `a` and located at `l`.
 type a @ l = Located l Async a
 
 -- | Effect signature for the `Choreo` monad.
-data Sig a where
+data ChoreoSig a where
   Comm ::
     (Show a, Read a) =>
     Loc s ->
     Loc r ->
     Located s Async a ->
-    Sig (Located r Async a)
+    ChoreoSig (Located r Async a)
   Cond ::
     (Show a, Read a) =>
     Loc s ->
     Located s Async a ->
     (a -> Choreo b) ->
-    Sig b
+    ChoreoSig b
   -- a variant of `Comm` where the sender and receiver are the same and the
   -- result does not need to be serializable
   Local ::
     Loc l ->
     Located l Async a ->
-    Sig (Located l Async a)
+    ChoreoSig (Located l Async a)
 
 -- | The monad for choreographies.
-newtype Choreo a = Choreo {unChoreo :: Freer Sig a}
+newtype Choreo a = Choreo {unChoreo :: Freer ChoreoSig a}
   deriving (Functor, Applicative, Monad)
 
 -- | Communication between a sender and a receiver.
@@ -77,18 +76,6 @@ locally ::
   Located l Async a ->
   Choreo (Located l Async a)
 locally l a = Choreo $ perform $ Local l a
-
--- * Endpoint projection
-
--- type SeqIdMap = HashMap LocTm SeqId
-
--- newSeqId :: (Monad m) => Loc l -> StateT SeqIdMap m SeqId
--- newSeqId l = do
---   m <- get
---   let l' = toLocTm l
---   let v = HM.findWithDefault 0 l' m
---   modify $ insert l' (v + 1)
---   return v
 
 -- handler :: (MonadIO m) => Loc t -> ChoreoSig fut m a -> StateT SeqIdMap (Network fut m) a
 -- handler t (Comm s r a) = undefined
@@ -131,5 +118,52 @@ locally l a = Choreo $ perform $ Local l a
 --       return (present v)
 --   | otherwise = return absent
 
-epp :: Choreo a -> Loc l -> Network a
-epp c t = undefined -- evalStateT (interp (handler t) (unChoreo c)) HM.empty
+-- | The state of `epp`.
+data EppState = EppState
+  { seqIdMap :: HashMap LocTm SeqId,
+    -- TODO: Use a heterogeneous list so that we don't need to create dummy
+    -- `Async`s
+    asyncList :: [Async ()]
+  }
+
+-- | Get the next sequence ID for a location (return 0 if there's none) and set
+-- the next sequence ID to the current value plus 1.
+newSeqId :: (MonadState EppState m) => Loc l -> m SeqId
+newSeqId l = do
+  s <- get
+  let l' = toLocTm l
+  let v = HM.findWithDefault 0 l' (seqIdMap s)
+  put $ s {seqIdMap = HM.insert l' (v + 1) (seqIdMap s)}
+  return v
+
+-- | Convert any `Async a` to `Async ()`.
+dummy :: Async a -> Async ()
+dummy = fmap (const ())
+
+-- | Add an `Async` to the list of `Async`s.
+addAsync :: (MonadState EppState m) => Async a -> m ()
+addAsync a = modify $ \s -> s {asyncList = dummy a : asyncList s}
+
+epp1 :: Choreo a -> Loc t -> StateT EppState Identity (Network a)
+epp1 = undefined
+
+epp :: Choreo a -> Loc t -> Network a
+epp c t =
+  let (n, s) = runIdentity $ runStateT (epp1 c t) (EppState HM.empty [])
+   in n >>= \a -> liftIO (runAsync $ sequenceA $ asyncList s) >> return a
+
+-- epp :: Choreo a -> Loc t -> Network a
+-- epp c t = evalStateT (interp (handler t) (unChoreo c)) HM.empty
+--   where
+--     handler :: Loc t -> Sig a -> StateT EppState Network a
+--     handler t (Comm s r a)
+--       | eqLoc s r = undefined
+--       | eqLoc t s = undefined
+--       | eqLoc t r = undefined
+--       | otherwise = undefined
+--     handler t (Cond s a f)
+--       | eqLoc t s = undefined
+--       | otherwise = undefined
+--     handler t (Local l a)
+--       | eqLoc t l = undefined
+--       | otherwise = undefined
