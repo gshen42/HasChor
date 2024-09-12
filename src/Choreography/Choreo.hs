b15a1e7f90bb89a03815243c2fa0b95ea1a6f090
Gan Shen <gan_shen@icloud.com>
2024-09-12 14:09:19 -0700

    very basic APIs

--- src/Choreography/Choreo.hs
+++ src/Choreography/Choreo.hs
@@ -1,36 +1,74 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GADTs #-}
 
 module Choreography.Choreo where
 
-import Choreography.Located
+import Choreography.Location
 import Choreography.Network
 import Control.Concurrent.Future
 import Control.Monad.Freer
 import Data.Kind
-import Data.Proxy
-
--- | Type-level list membership.
-type family Member (l :: Type) (ls :: [Type]) :: Constraint where
-  Member x (x : xs) = ()
-  Member x (y : xs) = Member x xs
+import Data.Typeable
 
 -- | Signature of the `Choreo` monad.
-data ChoreoSig ls m a where
+data ChoreoSig (ls :: [Loc]) (m :: Type -> Type) (a :: Type) where
+  Locally ::
+    forall l {ls} {m} {a}.
+    (Typeable l, Member l ls) =>
+    ((Unwrappable l) => m a) ->
+    ChoreoSig ls m (a @ l)
   Comm ::
-    (Member s ls, Member r ls, Show a, Read a) =>
-    Located s m a ->
-    ChoreoSig ls m (Located r m (Future a))
+    forall s r {ls} {m} {a}.
+    (Typeable s, Typeable r, Member s ls, Member r ls, Show a, Read a) =>
+    ((Unwrappable s) => m a) ->
+    ChoreoSig ls m (Future a @ r)
   Cond ::
-    (Member s ls) =>
-    Located s m a ->
+    forall s {b} {ls} {m} {a}.
+    (Typeable s, Member s ls) =>
+    ((Unwrappable s) => m a) ->
     (a -> Choreo ls m b) ->
     ChoreoSig ls m b
-  -- equivalent to `Comm` to oneself except the data doesn't need to be serializable
-  Locally :: (Member l ls) => Located l m a -> ChoreoSig ls m a
 
 -- | The monad for choreographies.
 type Choreo ls m a = Freer (ChoreoSig ls m) a
 
-epp :: Choreo ls m a -> Proxy l -> Network m a
-epp = undefined
+-- TODO: is there a way to avoid repeating the type signatures?
+
+locally ::
+  forall l {ls} {m} {a}.
+  (Typeable l, Member l ls) =>
+  ((Unwrappable l) => m a) ->
+  Choreo ls m (a @ l)
+locally act = perform (Locally act)
+
+comm ::
+  forall s r {ls} {m} {a}.
+  (Typeable s, Typeable r, Member s ls, Member r ls, Show a, Read a) =>
+  ((Unwrappable s) => m a) ->
+  Choreo ls m (Future a @ r)
+comm act = perform (Comm @s @r act)
+
+cond ::
+  forall s {b} {ls} {m} {a}.
+  (Typeable s, Member s ls) =>
+  ((Unwrappable s) => m a) ->
+  (a -> Choreo ls m b) ->
+  Choreo ls m b
+cond act k = perform (Cond @s act k)
+
+type ChoreoIO ls a = Choreo ls IO a
+
+printChoreo :: Choreo ls m a -> IO a
+printChoreo = interp handler
+  where
+    handler :: ChoreoSig ls m a -> IO a
+    handler (Comm @s @r a) = print (reify @s) >> print (reify @r) >> return (error "hhh")
+
+epp :: forall (t :: Loc) {ls} {m} {a}. (Typeable t) => Choreo ls m a -> Network m a
+epp = interp handler
+  where
+    handler :: forall ls m a. ChoreoSig ls m a -> Network m a
+    handler (Locally @l a) = undefined
+    handler (Comm @s @r a) = undefined
+    handler (Cond @s a k) = undefined
