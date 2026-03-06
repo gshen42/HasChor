2e4d89e7204a90345000414f0b4588ce7a9db777
Gan Shen <gan_shen@icloud.com>
2026-03-06 11:28:18 -0800

    set up the user-facing module

--- src/Choreography.hs
+++ src/Choreography.hs
@@ -1,43 +1,12 @@
 {-# LANGUAGE ExplicitNamespaces #-}
 
--- | This module defines the interface to HasChor. The client of the library is
--- highly recommended to only use constructs exported by this module.
 module Choreography (
-  -- * Locations and Located Values
-  LocTm,
-  LocTy,
-  type (@),
-  mkLoc,
-
-  -- * The Choreo monad
-  Choreo,
-  -- ** Choreo operations
-  locally,
-  (~>),
-  (~~>),
-  cond,
-  cond',
-
-  -- * Message transport backends
-  -- ** The HTTP backend
-  Host,
-  Port,
-  HttpConfig,
-   mkHttpConfig,
-
-  -- * Running choreographies
-  runChoreo,
-  runChoreography
+    type (@),
+    Choreo,
+    locally,
+    (~>),
+    (~~>),
+    cond,
   ) where
 
-import Choreography.Location
 import Choreography.Choreo
-import Choreography.Network
-import Choreography.Network.Http
-import Choreography.Network.Local
-import Control.Monad.IO.Class
-import Data.Proxy
-
--- | Run a choreography with a message transport backend.
-runChoreography :: (Backend config, MonadIO m) => config -> Choreo m a -> LocTm -> m a
-runChoreography cfg choreo l = runNetwork cfg l (epp choreo l)
