7b8a4e60faf20458d4345f60e515b434df48abde
Gan Shen <gan_shen@icloud.com>
2026-03-29 23:54:30 -0700

    finish the top-most module

--- src/Choreography.hs
+++ src/Choreography.hs
@@ -1,15 +1,41 @@
 {-# LANGUAGE ExplicitNamespaces #-}
 
+-- | This module defines the interface to HasChor.
 module Choreography (
-    type (@),
-    Choreo,
-    locally,
-    (~>),
-    (~~>),
-    cond,
-    par,
+  -- * Locations and Located Values
+  LocTm,
+  LocTy,
+  type (@),
+  mkLoc,
+
+  -- * The Choreo monad
+  Choreo,
+  -- ** Choreo operations
+  locally,
+  (~>),
+  (~~>),
+  cond,
+  par,
+
+  -- * Message transport backends
+  -- ** The HTTP backend
+  Host,
+  Port,
+  HttpConfig,
+  mkHttpConfig,
+
+  -- * Running choreographies
+  -- runChoreo,
+  runChoreography,
   ) where
 
 import Choreography.Location
 import Choreography.Choreo
+import Choreography.Network
+import Choreography.Network.Http
 import Control.Monad.Tree
+import Control.Monad.Reader
+
+-- | Run a choreography with a message transport backend.
+runChoreography :: (Backend config) => config -> Choreo IO a -> LocTm -> IO a
+runChoreography cfg c l = runNetwork cfg l (runReaderT (unEpp (epp c l)) Root)
